062dc62d122e52311d333a34f89e87c2f33a4f3d
Varsha <varsh6362@gmail.com>
2026-02-07 11:33:54 +0530

    orange

--- db.py
+++ db.py
@@ -1,14 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 base=declarative_base()
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL", DATABASE_URL)
+
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+
+sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_db():
-    db = create_engine(DATABASE_URL)
+    db = sessionlocal()
     try:
         yield db
     finally:
